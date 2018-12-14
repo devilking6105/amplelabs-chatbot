ec98b6c4f818533c3bd0ec196794049f82642c25
Daniel <chosww@gmail.com>
2018-12-14 05:14:05 -0500

    error with displaying on Google maps directions fixed

--- intents/lib/dialog-actions.js
+++ intents/lib/dialog-actions.js
@@ -67,12 +67,16 @@ class DialogActions {
     content,
     mealAddress
   ) {
+    let intersection =
+      slots.Intersection == null
+        ? null
+        : slots.Intersection.replace("&", " and ");
     let directionsMap = `https://www.google.com/maps/dir/?api=1&origin=${
       slots.useGPS === "Yes" &&
       slots.Latitude != null &&
       slots.Longitude != null
         ? `${slots.Latitude},${slots.Longitude}`
-        : `${slots.Intersection.replace("&", " and ")}%20Toronto`
+        : `${intersection}%20Toronto`
     }&destination=${mealAddress}&travelmode=walking`;
 
     return {
