2d3679c3644dc26789fa87071d1999c4513febcb
Daniel <chosww@gmail.com>
2019-02-18 20:00:16 -0500

    random async removed

--- intents/lib/data-loader.js
+++ intents/lib/data-loader.js
@@ -2,7 +2,7 @@ const mealInfoFile = require("../../data/meal.json");
 const Meal = require("./meal");
 
 module.exports = {
-  meals: async (mealInfo = mealInfoFile) => {
+  meals: (mealInfo = mealInfoFile) => {
     const concat = (x, y) => x.concat(y);
     const flatMap = (xs, f) => xs.map(f).reduce(concat, []);
     return flatMap(mealInfo, orgInfo => Meal.fromJson(orgInfo));
