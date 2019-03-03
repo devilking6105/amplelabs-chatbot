cd8242e20758aefc7d5627714eaa076846a9c1d9
Matthew Wong <matwong@zoho.com>
2019-03-03 08:37:25 -0500

    added testes clases for time/loc, age and gender

--- intents/lib/meal-finder.spec.js
+++ intents/lib/meal-finder.spec.js
@@ -5,27 +5,29 @@ const MealFinder = require('./meal-finder')
 
 const mealInfoFile = require("../../data/mock-meals-50.json");
 
-
-// const coords = (latitude, longitude) => {
-//   return {latitude, longitude, city: 'Toronto'}
-// }
-
-
-describe('MealFinder', async () => {
-  it(
+describe('MealFinder -- time and location', async () => {
+  it.each([
+    ['35196-2'],
+    ['35234-2'],
+    ['35242-2'],
+    ['35214-2'],
+    ['35323-2'],
+    ['35213-2'], // this one fails -- over night
+    ['35309-2']
+  ])(
     `Given a address of a service provider,
     it should return the same meal record of the service provider.`,
-    async () => {
-      const meals = await dataLoader.meals(mealInfoFile) // why dataloader returns a promise?
-      const correctResId = '35196-2'
+    async (rec) => {
+      const meals = await dataLoader.meals(mealInfoFile)
+      const correctResId = rec
       const closestMeal = meals.find(m => m.resourceId === correctResId)
       const location = await Location.fromAddress(closestMeal.address)
       const datetime = moment()
         .day(closestMeal.dayOfWeek[0])
         .hour(closestMeal.startTime.split(':')[0])
         .minute(closestMeal.startTime.split(':')[1])
-      const age = 19
-      const gender = 'mix'
+      const age = ''
+      const gender = closestMeal.gender
     
       // console.log(`ask meal time at time: ${datetime.format()}`)
     
@@ -37,91 +39,160 @@ describe('MealFinder', async () => {
         age, 
         gender)
       const result = await mealFinder.find()
-      // const location = await Location.fromCoords(coords(43.652088, -79.385525))
-      // const closestMeal = new Meal(coords(43.652088, -79.385525))
-      //console.log(location)
-      expect(result[0].resourceId).toEqual(correctResId)
-  })
-  // it('finds the closest meal', async () => {
-  //   // Ref: "resourceId": "35196-2",
-  //   const location = await Location.fromCoords(coords(43.652088, -79.385525))
-
-  //   const closestMeal = new Meal(coords(43.652088, -79.385525))
-  //   const fartherMeal = new Meal(coords(43.752088, -79.485525)) // make up gps
-  //   const meals = [fartherMeal, closestMeal]
-
-  //   const mealFinder = new MealFinder(meals, location)
-  //   console.log(mealFinder.find)
-  //   console.log(closestMeal)
-
-  //   expect(mealFinder.find(1)).toEqual([closestMeal])
-  // })
-})
-
-
-/*
-const Meal = require('./meal')
-const Location = require('./location')
-const MealFinder = require('./meal-finder')
-
-const coords = (latitude, longitude) => {
-  return {latitude, longitude, city: 'Toronto'}
-}
-
-describe('MealFinder', () => {
-  it('finds the closest meal', () => {
-    const location = new Location(coords(1, 1))
-
-    const closestMeal = new Meal(coords(2, 2))
-    const fartherMeal = new Meal(coords(10, 10))
-    const meals = [fartherMeal, closestMeal]
-
-    const mealFinder = new MealFinder(meals, location)
-
-    expect(mealFinder.find(1)).toEqual([closestMeal])
-  })
-
-  it('returns the specified number of meals', () => {
-    const location = new Location(coords(1, 1))
-
-    const closestMeal = new Meal(coords(2, 2))
-    const closerMeal = new Meal(coords(3, 3))
-    const fartherMeal = new Meal(coords(10, 10))
-    const meals = [closestMeal, fartherMeal, closerMeal]
-
-    const mealFinder = new MealFinder(meals, location)
-    const foundMeals = mealFinder.find(2)
-
-    expect(foundMeals).toEqual([closestMeal, closerMeal])
-  })
-
-  it('returns no meals if none are available', () => {
-    const location = new Location(coords(1, 1))
-
-    const mealFinder = new MealFinder([], location)
-    const foundMeals = mealFinder.find(2)
-
-    expect(foundMeals).toEqual([])
-  })
-
-  it('returns no meals if the specified location is unknown', () => {
-    const location = new Location({})
-
-    const meals = [new Meal(coords(2, 2))]
-    const mealFinder = new MealFinder(meals, location)
-    const foundMeals = mealFinder.find(1)
-
-    expect(foundMeals).toEqual([])
-  })
-
-  it('returns no meals if the specified location is outside Toronto', () => {
-    const location = new Location({...coords(1, 1), city: 'Not Toronto'})
-
-    const meals = [new Meal(coords(2, 2))]
-    const mealFinder = new MealFinder(meals, location)
-    const foundMeals = mealFinder.find(1)
-
-    expect(foundMeals).toEqual([])
+      expect(result[0]).toEqual(closestMeal)
   })
 })
-*/
+
+describe('MealFinder -- Age', async () => {
+  it.each([
+    ['35311-2'] /* this one failed for no reason */, ['35244-2'], ['35230-2'], ['35237-2'], ['35152-2'],
+    ['35300-2'] /* another overnight one */, ['35278-2'], ['35312-2'],
+    ['35279-2'], ['35318-2'] /* another overnight one */
+  ])(
+    `Given a address of a service provider with AGE restriction,
+    it should return the same meal record of the service provider.`,
+    async (rec) => {
+    const meals = await dataLoader.meals(mealInfoFile)
+    const correctResId = rec
+    const closestMeal = meals.find(m => m.resourceId === correctResId)
+    const location = await Location.fromAddress(closestMeal.address)
+    const datetime = moment()
+        .day(closestMeal.dayOfWeek[0])
+        .hour(closestMeal.startTime.split(':')[0])
+        .minute(closestMeal.startTime.split(':')[1])
+    const age = closestMeal.age[0] + 1
+    
+    const gender = closestMeal.gender
+    const mealFinder = new MealFinder(
+      meals, 
+      location,
+      datetime.format("ddd").toLowerCase(), 
+      datetime.format("HH:mm"), 
+      age, 
+      gender)
+    const result = await mealFinder.find()
+    // console.log(result)
+    // expect(result[0].resourceId).toEqual(correctResId)
+    expect(result[0]).toEqual(closestMeal)
+  })
+})
+
+describe('MealFinder -- Gender', async () => {
+  it.each([['35144-2'], ['35330-2'], ['35282-2'], ['35152-2'], ['35240-2']])(
+    `Given a address of a service provider with FEMALE restriction,
+    it should return the same meal record of the service provider.`,
+    async (rec) => {
+
+    const meals = await dataLoader.meals(mealInfoFile)
+    const correctResId = rec
+    const closestMeal = meals.find(m => m.resourceId === correctResId)
+    const location = await Location.fromAddress(closestMeal.address)
+    const datetime = moment()
+        .day(closestMeal.dayOfWeek[0])
+        .hour(closestMeal.startTime.split(':')[0])
+        .minute(closestMeal.startTime.split(':')[1])
+    const age = closestMeal.age[0] + 1
+    
+    const gender = closestMeal.gender
+    const mealFinder = new MealFinder(
+      meals, 
+      location,
+      datetime.format("ddd").toLowerCase(), 
+      datetime.format("HH:mm"), 
+      age, 
+      gender)
+    const result = await mealFinder.find()
+    expect(result[0]).toEqual(closestMeal)
+  })
+
+  // all records in the mock records with male restriction
+  it.each([['35100-2'], ['35338-2']])(
+    `Given a address of a service provider with Male restriction,
+    it should return the same meal record of the service provider.`,
+    async (rec) => {
+
+    const meals = await dataLoader.meals(mealInfoFile)
+    const correctResId = rec
+    const closestMeal = meals.find(m => m.resourceId === correctResId)
+    const location = await Location.fromAddress(closestMeal.address)
+    const datetime = moment()
+        .day(closestMeal.dayOfWeek[0])
+        .hour(closestMeal.startTime.split(':')[0])
+        .minute(closestMeal.startTime.split(':')[1])
+    const age = ''
+    
+    const gender = closestMeal.gender
+    const mealFinder = new MealFinder(
+      meals, 
+      location,
+      datetime.format("ddd").toLowerCase(), 
+      datetime.format("HH:mm"), 
+      age, 
+      gender)
+    const result = await mealFinder.find()
+    // console.log(result)
+    // expect(result[0].resourceId).toEqual(correctResId)
+    expect(result[0]).toEqual(closestMeal)
+  })
+
+  // all records in the mock records with OTHER restriction
+  it.each([['35314-2'], ['35135-2']])(
+    `Given a address of a service provider with genter as OTHER restriction,
+    it should return the same meal record of the service provider.`,
+    async (rec) => {
+
+    const meals = await dataLoader.meals(mealInfoFile)
+    const correctResId = rec
+    const closestMeal = meals.find(m => m.resourceId === correctResId)
+    const location = await Location.fromAddress(closestMeal.address)
+    const datetime = moment()
+        .day(closestMeal.dayOfWeek[0])
+        .hour(closestMeal.startTime.split(':')[0])
+        .minute(closestMeal.startTime.split(':')[1])
+    const age = ''
+    
+    const gender = closestMeal.gender
+    const mealFinder = new MealFinder(
+      meals, 
+      location,
+      datetime.format("ddd").toLowerCase(), 
+      datetime.format("HH:mm"), 
+      age, 
+      gender)
+    const result = await mealFinder.find()
+    // console.log(result)
+    // expect(result[0].resourceId).toEqual(correctResId)
+    expect(result[0]).toEqual(closestMeal)
+  })
+})
+
+describe('MealFinder -- Age and Gender', async () => {
+    // all records in the mock records with age and gender restriction
+    it.each([['35279-2'], ['35152-2']])(
+      `Given a address of a service provider with both GENDER & AGE restriction,
+      it should return the same meal record of the service provider.`,
+      async (rec) => {
+      const reordsWithRestrictions = rec
+  
+      const meals = await dataLoader.meals(mealInfoFile)
+      const correctResId = rec
+      const closestMeal = meals.find(m => m.resourceId === correctResId)
+      const location = await Location.fromAddress(closestMeal.address)
+      const datetime = moment()
+          .day(closestMeal.dayOfWeek[0])
+          .hour(closestMeal.startTime.split(':')[0])
+          .minute(closestMeal.startTime.split(':')[1])
+      const age = closestMeal.age[0] + 1
+      
+      const gender = closestMeal.gender
+      const mealFinder = new MealFinder(
+        meals, 
+        location,
+        datetime.format("ddd").toLowerCase(), 
+        datetime.format("HH:mm"), 
+        age, 
+        gender)
+      const result = await mealFinder.find()
+      expect(result[0]).toEqual(closestMeal)
+    })
+})
